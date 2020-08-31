2c8283a0c516ca68c4f14fbd538920de7bc51b37
Pierre-Claver Koko Banywerha <pierreclaverkoko@gmail.com>
2020-08-31 13:12:25 +0200

    update

--- projet/settings.py
+++ projet/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'projet.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bloodbankdb',
-        'USER': 'joelle',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        
-    }
-}
-
 #DATABASES = {
     #'default': {
-       #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
+       # 'ENGINE': 'django.db.backends.postgresql',
+       # 'NAME': 'bloodbankdb',
+       # 'USER': 'joelle',
+        #'PASSWORD': 'password',
+       # 'HOST': 'localhost',
+        
+   # }
 #}
+
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
